f5961d66ff7b3a36a1ec475e286f9ad18e2e9c97
AADI01010 <153974550+AADI01010@users.noreply.github.com>
2025-02-02 15:20:59 +0530

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,9 +1,5 @@
 package Shapes;
-
 public interface Shape {
     double doubleArea();
     double doublePerimeter();
 }
-
-//2	Define a package shapes containing an interface Shape with methods double area() and double perimeter()
-// . Implement the interface in classes Circle and Rectangle
